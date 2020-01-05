7bcc78550c8d84938fa49c26423d7c37dd6ed551
Emery Tate <tateejt@gmail.com>
2020-01-05 20:50:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   
   def create 
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-  redirect_to posts_path
+  redirect_to student_path
   end
 
 end
